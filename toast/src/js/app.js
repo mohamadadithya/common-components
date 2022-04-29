cb3de29daeddf40d56758c02771dd9df28e28c98
mohamadadithya <mohamadadithya@gmail.com>
2022-04-30 00:16:12 +0700

    clean the codes

--- toast/src/js/app.js
+++ toast/src/js/app.js
@@ -3,26 +3,21 @@ const toast = document.querySelector('.toast');
 
 const showToast = (status, time) => {
     const toastIcon = toast.querySelector('.toast__icon');
+    toast.classList.add(status, 'show');
     switch (status) {
         case 'success':
-            toast.classList.add('success');
             toastIcon.classList.add('fa-check');
             break;
         case 'error':
-            toast.classList.add('error');
             toastIcon.classList.add('fa-times');
             break;
         case 'warning':
-            toast.classList.add('warning');
             toastIcon.classList.add('fa-exclamation-triangle');
             break;
         default:
-            toast.classList.add('info');
             toastIcon.classList.add('fa-info');
             break;
     }
-
-    toast.classList.add('show');
     setTimeout(() => {
         toast.classList.remove('show')
     }, time)
